0b51358055f7316e66b6e8cae88591c1854cfbbc
Julia <juliablaumann@gmail.com>
2026-06-12 17:30:04 +0200

    viewing tasks; task cross out

--- cli-task-manager/smart_planner/planner/urls.py
+++ cli-task-manager/smart_planner/planner/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("tasks/", views.task_list, name="task_list"),
     path("tasks/<int:n>/delete", views.delete_task, name="delete_task"),
+    path("tasks/add/", views.add_task, name="add_task"),
+    path("tasks/<int:n>/done", views.status_done, name="status_done"),
 ]
--- cli-task-manager/smart_planner/planner/views.py
+++ cli-task-manager/smart_planner/planner/views.py
@@ -1,16 +1,51 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from datetime import date
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, world.")
+from .forms import AddTask
+from .models import Task
 
-def bank(request):
-    return HttpResponse("Twoje saldo wynosi 1 000$.")
 
-def hello(request, name):
-    template = "hello/hello_name.html"
-    ctx = {"name": name}
-    return render(request, template, ctx)
+def task_list(request):
+    tasks = Task.objects.filter(planned_date = date.today())
+    return render(request, "planner/tasks_list.html", {"tasks": tasks})
+
+"""
+Task.object.all() to mój model, czyli tabela w bazie danych <- pobierz wszystkie taski z tabeli Task
+objects to manager django do niego się zawsze zwracamy np. Tasks.objects.filter(...)
+render() mówi django Weź template HTML, włóż do niego dane i odeślij gotową stronę do przeglądarki.
+request - żądnanie użytkownika
+planner/tasks)list.html - plik html, który ma zostać pokazany
+{"tasks": tasks} - dane przekazane do HTMLa; słownik
+views.py:
+pobiera dane
+
+template:
+wyświetla dane
+"""
+
+def delete_task(request, n):
+    task = Task.objects.get(id=n)
+    task.delete()
+    return redirect("task_list")
+
+def status_done(request, n):
+    task = Task.objects.get(id=n)
+    task.status = "done"
+    task.save()
+    return redirect("task_list")
+
+def add_task(request):
+    if request.method == "POST":
+        form = AddTask(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['title']
+            priority = form.cleaned_data['priority']
+            planned_date = form.cleaned_data['planned_date']
+            duration_minutes = form.cleaned_data['duration_minutes']
+            status = form.cleaned_data['status']
+            Task.objects.create(name=name, priority=priority, planned_date=planned_date, duration_minutes=duration_minutes,status=status)
+            return redirect("task_list")
+    else:
+        form = AddTask()
+    return render(request, "planner/forms.html", {"form": form})
